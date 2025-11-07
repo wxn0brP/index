bbb33910e13c06dbd52ea091dc495a0b537c94c9
wxn0brP <toja.filiper@gmail.com>
2025-11-07 16:40:42 +0100

    feat: icons

--- src/render.ts
+++ src/render.ts
@@ -2,6 +2,16 @@ import { RepoData } from "./types";
 
 const main = document.querySelector("main")!;
 
+const langMap: Record<string, string> = {
+    "C++": "cplusplus",
+    "C#": "csharp",
+    "CSS": "css3",
+    "HTML": "html5",
+    "Shell": "bash",
+};
+
+const link = (name: string) => `https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${name}/${name}-original.svg`;
+
 export function renderProjects(data: [string, RepoData[]][]) {
     let html = ``;
     for (const obj of data) {
@@ -10,7 +20,13 @@ export function renderProjects(data: [string, RepoData[]][]) {
             html += `<h2>${category}</h2>`;
             html += `<ul>`;
             for (const repo of repos) {
-                html += `<li><a href="${repo.html_url}">${repo.name}</a> - ${repo.description}</li>`;
+                const lang = repo.language ? langMap[repo.language] || repo.language.toLowerCase() : "";
+                const icon = `<img class="lang-icon" src="${link(lang || "markdown")}">`;
+                html += `<li>
+                    ${icon}
+                    <a href="${repo.html_url}">${repo.name}</a> -
+                    ${repo.description}
+                </li>`;
             }
             html += `</ul>`;
         }
