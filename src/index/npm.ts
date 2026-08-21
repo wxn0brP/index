88ba2388d16085ef990d9bbc357b0ee00b941785
wxn0brP <viviennevaltheris@gmail.com>
2026-08-21 16:56:38 +0200

    fix: normalizeNpmPackageName

--- src/index/npm.ts
+++ src/index/npm.ts
@@ -1,6 +1,24 @@
+const EXCEPTIONS: Record<string, string> = {
+	"biome-config": "biome",
+	AnotherCache: "ac",
+	"GlovesLink-server-limit": "gls-limit",
+	VQL: "vql",
+};
+
+const PREFIXES: Record<string, string> = {
+	ValtheraDB: "db",
+};
+
 export function normalizeNpmPackageName(name: string) {
-	if (name.startsWith("ValtheraDB")) name = name.replace("ValtheraDB", "db");
-	if (name.match(/[A-Z]/))
-		name = name[0] + name.slice(1).replace(/([A-Z])/g, "-$1");
+	if (name in EXCEPTIONS) return `@wxn0brp/${EXCEPTIONS[name]}`;
+
+	for (const [prefix, replacement] of Object.entries(PREFIXES)) {
+		if (name.startsWith(prefix)) {
+			name = replacement + name.slice(prefix.length);
+			break;
+		}
+	}
+
+	name = name.replace(/([a-z0-9])([A-Z])/g, "$1-$2");
 	return `@wxn0brp/${name.toLowerCase()}`;
 }
